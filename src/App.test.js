8a30a00eb9165814dd62ff063af6b1fb40ea0f70
lulucodingwizard <how.to.develop.fast@gmail.com>
2024-06-24 13:35:27 +1000

    feat: add unit test for Links

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("render my heading title", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    const titleElement = screen.getByText(/My Awesome Posts/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("renders Home and Posts links", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const homeLink = screen.getByRole("link", { name: /Home/i });
+    const postsLink = screen.getByRole("link", { name: /Posts/i });
+    expect(homeLink).toBeInTheDocument();
+    expect(postsLink).toBeInTheDocument();
+  });
+
+  test("navigates to Home page on link click", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const homeLink = screen.getByRole("link", { name: /Home/i });
+    userEvent.click(homeLink);
+    const homePageElement = screen.getByText(/Welcome to the React Router v6/i);
+    expect(homePageElement).toBeInTheDocument();
+  });
+
+  test("navigates to Posts page on link click", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const postsLink = screen.getByRole("link", { name: /Posts/i });
+    waitFor(() => {
+      userEvent.click(postsLink);
+      const postsPageElement = screen.getByText(/Posts/i);
+      expect(postsPageElement).toBeInTheDocument();
+    });
+  });
 });
